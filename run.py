08e35b0d58345f1d4e02843d81d6c074110fa5dd
wenderccc <49318486+wenderccc@users.noreply.github.com>
2020-03-14 20:16:23 +0300

    v1.32

--- run.py
+++ run.py
@@ -9,55 +9,58 @@ from selenium.webdriver.chrome.options import Options
 
 
 def enable():
-    print('Вход в систему...')
-    global chrome_options
-    chrome_options = Options()
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument('--disable-audio-output')
-    global driver
-    driver = webdriver.Chrome(options=chrome_options)
-    with open('pass_yt.txt', 'r') as file:
-        passyt = file.read()
-    with open('log_yt.txt', 'r') as file:
-        logyt = file.read()
-    logyt = logyt.strip()
-    passyt = passyt.strip()
-    driver.get('https://ytmonster.ru/')
     while True:
+        print('Вход в систему...')
+        global chrome_options
+        chrome_options = Options()
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument('--disable-audio-output')
+        global driver
+        driver = webdriver.Chrome(options=chrome_options)
+        with open('pass_yt.txt', 'r') as file:
+            passyt = file.read()
+        with open('log_yt.txt', 'r') as file:
+            logyt = file.read()
+        logyt = logyt.strip()
+        passyt = passyt.strip()
+        driver.get('https://ytmonster.ru/')
+        while True:
+            try:
+                driver.find_element_by_css_selector(
+                    'body > table > tbody > tr > td > div.cf-browser-verification.cf-im-under-attack')
+                sleep(2)
+            except:
+                break
+        sleep(1)
+        while True:
+            try:
+                driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/div[2]/ul/li[2]/a').click()
+                break
+            except:
+                driver.refresh()
+        sleep(1)
+        driver.find_element_by_xpath('/html/body/div[6]/div[2]/div[1]/input[1]').send_keys(logyt)
+        driver.find_element_by_xpath('/html/body/div[6]/div[2]/div[1]/input[2]').send_keys(passyt)
+        sleep(1)
+        driver.find_element_by_xpath('//*[@id="booSubmit"]').click()
+        sleep(5)
+        driver.get('https://www.youtube.com/')
+        cookies = pickle.load(open("cookiesgoogle.pkl", "rb"))
+        for cookie in cookies:
+            if 'expiry' in cookie:
+                del cookie['expiry']
+                driver.add_cookie(cookie)
+        driver.refresh()
+        driver.get('https://ytmonster.ru/task')
         try:
-            driver.find_element_by_css_selector('body > table > tbody > tr > td > div.cf-browser-verification.cf-im-under-attack')
-            sleep(2)
+            driver.find_element_by_css_selector('#news > div > div > div > div.col-lg-2.ml-lg-auto > div > img')
+            print('Вход в YtMonster не был произведен, начинаем программу заново...')
+            continue
         except:
-            break
-    sleep(1)
-    while True:
-        try:
-            driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/div[2]/ul/li[2]/a').click()
-            break
-        except:
-            driver.refresh()
-    sleep(1)
-    driver.find_element_by_xpath('/html/body/div[6]/div[2]/div[1]/input[1]').send_keys(logyt)
-    driver.find_element_by_xpath('/html/body/div[6]/div[2]/div[1]/input[2]').send_keys(passyt)
-    sleep(1)
-    driver.find_element_by_xpath('//*[@id="booSubmit"]').click()
-    sleep(5)
-    driver.get('https://www.youtube.com/')
-    cookies = pickle.load(open("cookiesgoogle.pkl", "rb"))
-    for cookie in cookies:
-        if 'expiry' in cookie:
-            del cookie['expiry']
-            driver.add_cookie(cookie)
-    driver.refresh()
-    driver.get('https://ytmonster.ru/task')
-    try:
-        driver.find_element_by_css_selector('#news > div > div > div > div.col-lg-2.ml-lg-auto > div > img')
-        print('Вход в YtMonster не был произведен, начинаем программу заново...')
-        return
-    except:
-        pass
-    print('Вход успешен!')
+            pass
+        print('Вход успешен!')
+        break
 
 def subs():
     driver.find_element_by_xpath('//*[@id="choseTaskType2"]/div').click()
@@ -90,7 +93,7 @@ def subs():
         else:
             pass
         driver.find_element_by_css_selector('#subscribe-button > ytd-subscribe-button-renderer > paper-button').click()
-        sleep(35)
+        sleep(59)
         driver.close()
         print('Задание выполенено!')
         driver.switch_to.window(handles[0])
@@ -121,14 +124,15 @@ def likes():
         return
     print('Задание началось')
     sleep(1)
-    driver.find_element_by_css_selector('#top-level-buttons > ytd-toggle-button-renderer:nth-child(1)').click()
-    sleep(40)
+    driver.find_element_by_css_selector('#top-level-buttons > ytd-toggle-button-renderer:nth-child(1) > a').click()
+    sleep(59)
     driver.close()
     print('Задание выполенено!')
     driver.switch_to.window(handles[0])
     sleep(2)
 
 print('Программа запускается')
+
 enable()
 while True:
     subs()
